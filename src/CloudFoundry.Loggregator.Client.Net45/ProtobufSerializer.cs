40d39dec771d0108919857aa958bd22db5cb17fd
Stefan Schneider <stefan.schneider@hp.com>
2015-07-17 03:45:23 -0700

    Add optional for some Loggregator protobuf fields

--- src/CloudFoundry.Loggregator.Client.Net45/ProtobufSerializer.cs
+++ src/CloudFoundry.Loggregator.Client.Net45/ProtobufSerializer.cs
@@ -14,17 +14,27 @@
         /// <returns>An ApplicationLog instance</returns>
         public ApplicationLog DeserializeApplicationLog(byte[] data)
         {
+            //// https://github.com/cloudfoundry/loggregatorlib/blob/master/logmessage/log_message.pb.go#L52
+
             RuntimeTypeModel model = RuntimeTypeModel.Create();
             Type applicationLogType = typeof(ApplicationLog);
             model.Add(applicationLogType, true);
             model[applicationLogType].Add(1, "Message");
+
             model[applicationLogType].Add(2, "LogMessageType");
+
             model[applicationLogType].Add(3, "Timestamp");
-            model[applicationLogType].Add(4, "AppId");
-            model[applicationLogType].Add(6, "SourceId");
-            model[applicationLogType].Add(7, "DrainUrls");
-            model[applicationLogType].Add(8, "SourceName");
             model[applicationLogType][3].DataFormat = DataFormat.ZigZag;
+
+            model[applicationLogType].Add(4, "AppId");
+
+            model[applicationLogType].Add(6, "SourceId");
+            model[applicationLogType][6].IsRequired = false;
+
+            model[applicationLogType].Add(7, "DrainUrls");
+
+            model[applicationLogType].Add(8, "SourceName");
+            model[applicationLogType][8].IsRequired = false;
 
             ApplicationLog log = null;
 
